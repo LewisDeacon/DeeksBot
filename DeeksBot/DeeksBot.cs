4fa1a159f09b23cf4d675a598bf9bd7bfdbede95
lewis.deacon <lewisdeaconx@gmail.com>
2017-05-20 19:05:18 +0100

    Start of the discord bot, some basic commands implemented

---
+++ DeeksBot/DeeksBot.cs
@@ -0,0 +1,109 @@
+﻿namespace DeeksBot
+{
+    using Discord;
+    using Discord.Commands;
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class DeeksBot
+    {
+        DiscordClient client;
+        CommandService commands;
+        private const string botToken = "ADD BOT TOKEN HERE";
+
+        public DeeksBot()
+        {
+            client = new DiscordClient(input =>
+            {
+                input.LogLevel = LogSeverity.Info;
+                input.LogHandler = Log;
+            });
+
+            client.UsingCommands(input => 
+            {
+                input.PrefixChar = '!';
+                input.AllowMentionPrefix = true;
+            });
+
+            commands = client.GetService<CommandService>();
+
+            RegisterSteamStatusCommand();
+            OsrsHighScoreChecker();
+            DisplaySourceCommand();
+
+            client.ExecuteAndWait(async () =>
+            {
+                await client.Connect(botToken, TokenType.Bot);
+            });
+        }
+
+        private void OsrsHighScoreChecker()
+        {
+            commands.CreateCommand("osrs").Parameter("name", ParameterType.Multiple).Do(async (e) =>
+            {
+                var highscores = GetOsrsHighScore(e);
+                await e.Channel.SendMessage($"Highscores for {e.Args[0]}: \n{highscores}");
+            });
+        }
+
+        private string GetOsrsHighScore(CommandEventArgs e)
+        {
+            var username = e.Args[0];
+            string result;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + username);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                result = reader.ReadToEnd();
+            }
+
+            RunescapePlayer rsPlayer = new RunescapePlayer(username);
+
+            var splitResult = result.Split(new Char[] { ',', '\n' });
+
+            int i = 0;
+            string rank;
+            string level;
+            string xp;
+
+            foreach (Skill x in Enum.GetValues(typeof(Skill)))
+            {
+                rank = splitResult.GetValue(i).ToString().Equals("-1") ? "N/A" : splitResult.GetValue(i).ToString();
+                level = splitResult.GetValue(i+1).ToString().Equals("-1") ? "N/A" : splitResult.GetValue(i+1).ToString();
+                xp = splitResult.GetValue(i+2).ToString().Equals("-1") ? "N/A" : splitResult.GetValue(i+2).ToString();
+
+
+                rsPlayer.PlayerSkillRank.Add(x, rank);
+                rsPlayer.PlayerSkillLevel.Add(x, level);
+                rsPlayer.PlayerSkillXp.Add(x, xp);
+                i+=3;
+            }
+            return rsPlayer.RunescapePlayerInfoFormatted();
+        }
+       
+        private void DisplaySourceCommand()
+        {
+            commands.CreateCommand("Source").Do(async (e) =>
+            {
+                await e.Channel.SendMessage("http://downdetector.com/status/steam");
+            });
+        }
+        private void RegisterSteamStatusCommand()
+        {
+            commands.CreateCommand("Steam").Do(async (e) =>
+            {
+                await e.Channel.SendMessage("http://downdetector.com/status/steam");
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ DeeksBot/RunescapePlayer.cs
@@ -0,0 +1,71 @@
+﻿using DeeksBot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeeksBot
+{
+    public enum Skill
+    {
+        Overall,
+        Attack,
+        Defence,
+        Strength,
+        Hitpoints,
+        Ranged,
+        Prayer,
+        Magic,
+        Cooking,
+        Woodcutting,
+        Fletching,
+        Fishing,
+        Firemaking,
+        Crafting,
+        Smithing,
+        Mining,
+        Herblore,
+        Agility,
+        Thieving,
+        Slayer,
+        Farming,
+        Runecraft,
+        Hunter,
+        Construction
+    }
+
+
+    public class RunescapePlayer
+    {
+        public RunescapePlayer(string username)
+        {
+            this.Username = username;
+            this.PlayerSkillRank = new Dictionary<Skill, string>();
+            this.PlayerSkillLevel = new Dictionary<Skill, string>();
+            this.PlayerSkillXp = new Dictionary<Skill, string>();
+        }
+
+        public string Username { get; set; }
+
+        public IDictionary<Skill, string> PlayerSkillRank { get; set; }
+        public IDictionary<Skill, string> PlayerSkillLevel { get; set; }
+        public IDictionary<Skill, string> PlayerSkillXp { get; set; }
+
+        public string RunescapePlayerInfoFormatted()
+        {
+            string format = "{0,-15} {1,-10} {2,-8} {3,-15}";
+            StringBuilder formattedString = new StringBuilder();
+            formattedString.AppendFormat(format, "```Skill", "    Rank", "  Level", "     XP");
+            formattedString.AppendLine();
+            foreach (Skill x in Enum.GetValues(typeof(Skill)))
+            {
+                formattedString.AppendFormat(format, x.ToString(), PlayerSkillRank[x], PlayerSkillLevel[x], PlayerSkillXp[x]);
+                formattedString.AppendLine();
+            }
+
+            formattedString.Append("```");
+            return formattedString.ToString();
+        }
+    }
+}
